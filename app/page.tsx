424d766d3efb9105b6fff2be10b8f0f783d11932
Rishang <rishangkumar9693@gmail.com>
2025-03-04 21:54:53 +0530

    added dark  and light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { ModeToggle } from "@/components/ThemeToggler";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Button variant={"outline"} size={"lg"} >hello</Button>
+    <Button variant={"outline"} size={"lg"} >hello</Button> 
+    <ModeToggle/>
     </>
   );
 }
