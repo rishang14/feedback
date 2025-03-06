932a06129c25d3ea57945f05c2ede8d018f5ec91
Rishang <rishangkumar9693@gmail.com>
2025-03-07 00:03:46 +0530

    added card section

---
+++ app/dashboard/_comp/dashboardcard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+const Dashboardcard = () => {
+  return (
+   <Card>
+    <CardContent>
+        <p className='text-white'>hello </p>
+    </CardContent>
+   </Card>
+  )
+}
+
+export default Dashboardcard;
