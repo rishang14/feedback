62c40ef819597f6aa2e3ee38fb06c973c88db40c
Rishang <rishangkumar9693@gmail.com>
2025-04-22 20:24:34 +0530

    removed the experimental error

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,93 @@
-export const runtime = 'nodejs'
 
-import { NextRequest ,NextResponse} from "next/server";  
-import { auth } from "./auth";
 
 
 // const { auth } = NextAuth(authConfig);  
 
+// const protectedRoutes = ['/dashboard','/dashboard/:path*','dashboard/space/:path*']
+// const publicRoutes = ['/', '/signin,signup','/'] 
+
+// export default auth(async function middleware(req: NextRequest) {
+//   const session=  await auth(); 
+//   console.log(session,"session")
+//   const path = req.nextUrl.pathname; 
+//   const isProtectedRoute = protectedRoutes.includes(path);
+//   const isPublicRoute = publicRoutes.includes(path); 
+
+
+//   if (isProtectedRoute && !session?.user?.email) {
+//     return NextResponse.redirect(new URL('/signin', req.nextUrl))
+//   }
+ 
+//   if (
+//     isPublicRoute &&
+//     session?.user?.email &&
+//     !req.nextUrl.pathname.startsWith('/dashboard')
+//   ) {
+//     return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
+//   }
+ 
+//   return NextResponse.next()
+//   }) 
+
+
+
+
+//  export const config = {
+//     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+//   } 
+
+//   import { NextRequest, NextResponse } from 'next/server';
+// import { jwtVerify } from 'jose';
+
+// const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET); // same secret from NextAuth config
+
+// async function getToken(req: NextRequest) {
+//   const token = req.cookies.get('next-auth.session-token')?.value
+//     || req.cookies.get('__Secure-next-auth.session-token')?.value;
+
+//   if (!token) return null;
+
+//   try {
+//     const { payload } = await jwtVerify(token, secret);
+//     return payload;
+//   } catch (err) {
+//     return null;
+//   }
+// }
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from "next-auth/jwt";
+
 const protectedRoutes = ['/dashboard','/dashboard/:path*','dashboard/space/:path*']
 const publicRoutes = ['/', '/signin,signup','/'] 
 
-export default auth(async function middleware(req: NextRequest) {
-  const session=  await auth(); 
-  console.log(session,"session")
-  const path = req.nextUrl.pathname; 
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path); 
-
-
-  if (isProtectedRoute && !session?.user?.email) {
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const session = await getToken({
+    req,
+    secret:process.env.NEXTAUTH_SECRET,
+    salt:'authjs.session-token'
+  });
+  console.log(session,"sesion")
+  const isProtected = protectedRoutes.some(route => path.startsWith(route));
+  const isPublic = publicRoutes.includes(path);
+  // @ts-ignore
+   if (isProtected && !session?.user?.email) {
     return NextResponse.redirect(new URL('/signin', req.nextUrl))
   }
- 
   if (
-    isPublicRoute &&
+    isPublic &&
+    //  @ts-ignore
     session?.user?.email &&
     !req.nextUrl.pathname.startsWith('/dashboard')
   ) {
     return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
   }
  
-  return NextResponse.next()
-  }) 
 
 
+  return NextResponse.next();
+}
 
-
- export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  }
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, 
   },
-  experimental:{
-    nodeMiddleware:true
-  }
+
 };
 
 export default nextConfig;
