c7acc3f28cd14d28a3aed4f9b3cf4622cea20dde
Rishang <rishangkumar9693@gmail.com>
2025-05-02 01:57:27 +0530

    delete route completed

--- app/api/(profile)/deleteprofile/route.ts
+++ app/api/(profile)/deleteprofile/route.ts
@@ -2,7 +2,14 @@ import connectDB from "@/lib/db.connect";
 import authConfig from "@/lib/auth.config";
 import NextAuth from "next-auth";
 import { NextResponse } from "next/server";
-import { getUserByEmail, spaceidsContainsTestimonials, getspacesWthUserId } from "@/lib/helper";
+import {
+  getUserByEmail,
+  spaceidsContainsTestimonials,
+  getspacesWthUserId,
+  DeleteProfile,
+} from "@/lib/helper";
+import { redirect } from "next/navigation";
+import { url } from "inspector";
 
 const { auth } = NextAuth(authConfig);
 
@@ -17,15 +24,16 @@ export async function DELETE() {
   await connectDB();
   try {
     const email = session?.user?.email;
-    const user =await  getUserByEmail(email as string); 
-    const sapceids= await getspacesWthUserId(user._id as string); 
-    const spccontainTestimonials= await spaceidsContainsTestimonials(sapceids)
-    console.log(sapceids,"ids");  
-    console.log(spccontainTestimonials,"Testimonials"); 
-
-    
-    return NextResponse.json({ message: "User deleted" }, { status: 200 });
+    const user = await getUserByEmail(email as string);
+    const sapceids = await getspacesWthUserId(user._id as string);
+    const spccontainTestimonials = await spaceidsContainsTestimonials(sapceids);
+    console.log(sapceids, "ids");
+    console.log(spccontainTestimonials, "Testimonials");
+    await DeleteProfile(user._id as string, sapceids, spccontainTestimonials);
+    return NextResponse.redirect(new URL("/"));
+    // return NextResponse.json({ message: "User deleted" }, { status: 200 });
   } catch (error: any) {
-    console.log(error)
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
